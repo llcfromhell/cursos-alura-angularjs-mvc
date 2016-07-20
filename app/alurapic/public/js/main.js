00368ab3f162dd03d1d1966ba8d81f85a608995e
Lourenco Lima da Cunha <llc.from.hell@gmail.com>
2016-07-20 09:14:28 -0300

    metade aula 03

---
+++ app/alurapic/public/js/main.js
@@ -0,0 +1,8 @@
+angular.module('alurapic', []).controller('FotosController', function($scope){
+
+	$scope.foto = {
+		url : 'http://www.fundosanimais.com/Minis/leao.jpg',
+		titulo : 'Leão'
+	}
+
+});
