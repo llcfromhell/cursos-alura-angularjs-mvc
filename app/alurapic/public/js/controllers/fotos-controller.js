505f3cab3603a4760563082b844553a1ee85b42e
Lourenco Lima da Cunha <llc.from.hell@gmail.com>
2016-08-08 22:43:03 +0000

    aula 03 e 04

---
+++ app/alurapic/public/js/controllers/fotos-controller.js
@@ -0,0 +1,14 @@
+angular.module('FotosController',[]).controller('FotosController', function($scope, $http){
+
+	$scope.fotos = [];
+	$scope.filtro = '';
+	
+	$http.get('v1/fotos')
+		.success(function(fotos){
+			$scope.fotos = fotos;
+		})
+		.error(function(error){
+			console.log(error);
+		});
+
+})
--- app/alurapic/public/js/main.js
+++ app/alurapic/public/js/main.js
@@ -1,13 +1,1 @@
-angular.module('alurapic', ['minhasDiretivas']).controller('FotosController', function($scope, $http){
-
-	$scope.fotos = [];
-	
-	$http.get('v1/fotos')
-		.success(function(fotos){
-			$scope.fotos = fotos;
-		})
-		.error(function(error){
-			console.log(error);
-		});
-
-});
+angular.module('alurapic', ['minhasDiretivas', 'FotosController', 'ngAnimate']);
